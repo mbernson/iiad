a3f19bca73e548b6af286c497eaa11d97f4c1590
Mathijs Bernson <mathijs.bernson@gmail.com>
2015-06-16 15:45:14 +0200

    Fix some stuff

--- src/main/java/iiad/les1/SortingAndSearchingDemo.java
+++ src/main/java/iiad/les1/SortingAndSearchingDemo.java
@@ -5,7 +5,7 @@ import iiad.les1.searching.Searchable;
 import iiad.les1.sorting.BubbleSort;
 import iiad.les1.sorting.Sortable;
 
-public class Main {
+public class SortingAndSearchingDemo {
 
     public static void main(String[] args) {
         final Character[] rij = {'M', 'H', 'O', 'G', 'P', 'F', 'Z', 'D', 'A', 'K'};
@@ -15,13 +15,13 @@ public class Main {
 
         final long start, end;
         start = System.currentTimeMillis();
-//        Character result = search.search(rij, 'A');
+        int index = search.search(rij, 'A');
+        System.out.printf("Karakter A staat op index %d\n", index);
         final Character[] sorted = sort.sort(rij);
         end = System.currentTimeMillis();
 
         System.out.println("Done");
         printList(sorted);
-//        System.out.println(result);
         System.out.printf("Elapsed time: %d ms\n", (end - start));
     }
 
