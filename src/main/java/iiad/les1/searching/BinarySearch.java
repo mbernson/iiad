1daa216c653073256a70a2c58e152a0de6df4cd5
Mathijs Bernson <mathijs.bernson@gmail.com>
2015-06-18 10:59:24 +0200

    Stuff

--- src/main/java/iiad/les1/searching/BinarySearch.java
+++ src/main/java/iiad/les1/searching/BinarySearch.java
@@ -8,7 +8,10 @@ public class BinarySearch<T extends Comparable<T>> implements Searchable<T> {
             high = items.length - 1;
 
         while(low <= high) {
-
+//            int middle = low + (high - low) / 2;
+//            if      (target.compareTo(items[middle])) hi = mid - 1;
+//            else if (key > a[mid]) lo = mid + 1;
+//            else return mid;
         }
 
         return NOT_FOUND;
